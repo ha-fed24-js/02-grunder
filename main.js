4e2aa315253129b8074ba60a10447b6794bab4e7
David Andersson <lejonmanen@gmail.com>
2024-10-29 13:59:05 +0100

    Loop exempel

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log('Hello world')
+// console.log('Hello world')
 
 let variabel1
 variabel1 = 1
@@ -20,7 +20,7 @@ let z = (2 * (3 - 5))
 console.log(y)
 */
 
-
+/*
 let variabel3 = Number('123')
 console.log( 'Talet är: ' + '1' + '2' + '3' )
 console.log('Nästa tal: ' + (variabel3 + 1))
@@ -32,3 +32,32 @@ console.log((10 + 20) / 100)
 
 let variabel4 = Number('Kalle')
 console.log( variabel4 )
+*/
+
+// 11a Vad händer när koden körs?
+let length = 130
+if( length > 130 ) {
+    console.log('Du får åka Balder!')
+} else if( length == 130 ) {
+	console.log('Sträck på dig så får du åka Balder!')
+} else {
+	console.log('Du får inte åka :(')
+}
+
+// while, for
+let count = 0
+while( count < 5 ) { 
+	console.log('Loop', count)
+	count++  // count = count + 1
+}
+
+for( let i=0; i < 5;  i++ ) {
+	console.log('For-loop', i)
+}
+
+// Lägg ihop talen 1 till 10
+let sum = 0
+for( let x=1; x<=10; x++ ) {
+	sum = sum + x
+}
+console.log('Summan är: ', sum)
