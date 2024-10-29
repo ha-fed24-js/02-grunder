1b649fbd764b6ce851e67e25b718ca77a328ee15
David Andersson <lejonmanen@gmail.com>
2024-10-29 11:46:48 +0100

    Exempel

---
+++ exercises.js
@@ -0,0 +1,13 @@
+// 4a Vad skrivs ut av följande kod?
+// let variabel1 = 32
+// console.log(variabel1)
+
+// 4b Ändra variabeln, så att koden skriver ut 32 i stället.
+
+// 6
+let x = 'yellow'
+console.log(x)
+console.log('Färgen är: ' + x)
+console.log('Färgen är: ', x)
+console.log(`Färgen är: ${x}`)
+console.log('Färgen är: yellow')
---
+++ main.js
@@ -0,0 +1,34 @@
+console.log('Hello world')
+
+let variabel1
+variabel1 = 1
+let variabel2 = 2
+// = betyder tilldelning
+// == betyder jämförelse, villkor
+
+
+/*let x, y;  // deklarera variabler
+x = 1;  // tilldela variablerna värden
+y = 0;
+
+while (x <= 10) {
+	y = y + x;
+	x = x + 1;
+}
+let z = (2 * (3 - 5))
+
+console.log(y)
+*/
+
+
+let variabel3 = Number('123')
+console.log( 'Talet är: ' + '1' + '2' + '3' )
+console.log('Nästa tal: ' + (variabel3 + 1))
+
+// `Talet är: ${variabel3}`
+console.log(0.1 + 0.3)
+console.log(0.1 + 0.2)
+console.log((10 + 20) / 100)
+
+let variabel4 = Number('Kalle')
+console.log( variabel4 )
